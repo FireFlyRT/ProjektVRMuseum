8b47555e468a7e7391e975f47ed88c588563569e
dark1608 <106972746+dark1608@users.noreply.github.com>
2025-07-08 17:07:00 +0200

    aktueller Stand Marcel, Sorry Patrick...

--- Assets/Scripts/TeleportButton.cs
+++ Assets/Scripts/TeleportButton.cs
@@ -8,7 +8,6 @@ public class TeleportButton : MonoBehaviour
     [SerializeField]
     private float _triggerPoint;
 
-    [SerializeField]
     private PlayerController _playerController;
     private Vector3 _startPosition;
 
@@ -17,12 +16,25 @@ public class TeleportButton : MonoBehaviour
         _startPosition = transform.position;
     }
 
-    public void OnClick()
+    private void Update()
+    {
+        if (transform.position.x - _startPosition.x >= _triggerPoint)
+            if (_playerController != null)
+                OnClick(_playerController);
+    }
+
+    private void OnClick(IInteractor sender)
     {
         if (_teleportationPlace == null) 
             return;
 
-        _playerController.TeleportTo(_teleportationPlace);
+        sender.TeleportTo(_teleportationPlace);
         transform.position = _startPosition;
+        _playerController = null;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _playerController = collision.transform.gameObject.GetComponentInParent<PlayerController>();
     }
 }
